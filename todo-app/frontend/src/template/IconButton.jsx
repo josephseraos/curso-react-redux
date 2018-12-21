e5be8933b5570e4bc59ef9e237f7ca2cf4ec1560
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-21 13:44:34 -0300

    Renderização condicional (if). Seção 5, aula 54

--- todo-app/frontend/src/template/IconButton.jsx
+++ todo-app/frontend/src/template/IconButton.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import If from '../template/If'
 
-export default props => {
-    if (props.hide) {
-        return null
-    } else {
-        return (
-            <button className={`btn btn-${props.estilo}`} onClick={props.onClick} title={props.title}>
-                <i className={`fa fa-${props.icon}`}></i>
-            </button>
-        )
-    }
-}
+export default props => (
+    <If test={!props.hide}>
+        <button className={`btn btn-${props.estilo}`} onClick={props.onClick} title={props.title}>
+            <i className={`fa fa-${props.icon}`}></i>
+        </button>
+    </If>
+)
