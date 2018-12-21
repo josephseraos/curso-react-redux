5383250afd2f89c37ebbcbcb0c91c8476ed7c685
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-21 10:05:08 -0300

    Componente menu: Seção 5, aula 47

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import Todo from '../todo/Todo'
 import About from '../about/About'
+import Menu from '../template/Menu'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Todo />
-                <About />
+                <header>
+                    <Menu />
+                </header>
+                <main>
+                    <Todo />
+                    <About />
+                </main>
             </div>
         )
     }
---
+++ todo-app/frontend/src/template/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default props => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#/todos">
+            <i class="fas fa-calendar-check"></i> Navbar
+        </a>
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#/todos">Tarefas <span className="sr-only">(página atual)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/about">Sobre</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
