b08b27ef2a8b621a509ed8e99d81459d96c6f8b6
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-21 09:33:58 -0300

    Criação da pasta src vazia, index.jsx e App.jsx. Seção 5, Aula 45

---
+++ todo-app/frontend/src/main/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Todo app</h1>
+        )
+    }
+}
+
+export default App
