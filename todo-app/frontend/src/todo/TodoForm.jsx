9633bd7cabc9e324201fffd88fc75e2b170328fa
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-21 13:01:12 -0300

    Estrutura do formulário. Seção 5, aula 52

--- todo-app/frontend/src/todo/TodoForm.jsx
+++ todo-app/frontend/src/todo/TodoForm.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
 
 export default props =>
-    <h1>Form</h1>
+    <div>
+        <div className="form-row">
+            <div className="col-xs-12 col-sm-9 col-md-10">
+                <input type="text" className="form-control" placeholder="Nome" autoFocus />
+            </div>
+            <div className="col-xs-12 col-sm-3 col-md-2 text-center">
+                <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
+                {/* <button className="btn btn-default ml-2"><i className="fa fa-search"></i></button> */}
+            </div>
+        </div>
+    </div>
