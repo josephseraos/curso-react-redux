b2f7eb069fac20dcf498d1216de0ffe1996d0b6d
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-21 11:48:31 -0300

    Componentes TodoForm e TodoList. Seção 5, aula 50

--- todo-app/frontend/src/todo/Todo.jsx
+++ todo-app/frontend/src/todo/Todo.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PageHeader from '../template/PageHeader'
+
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
 
 class Todo extends Component {
     render() {
         return (
-            <PageHeader name='Tarefas' small='Cadastro' />
+            <Fragment>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm />
+                <TodoList />
+            </Fragment>
         )
     }
 }
