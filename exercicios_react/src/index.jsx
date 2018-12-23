50129bc3a90599bc2ae9f0fd82b1d72d9aec4c70
Joseph Soares Alcântara <josephseraos@gmail.com>
2018-12-23 14:29:40 -0300

    Configurando o redux. Seção 6, aula 65

--- exercicios_react/src/index.jsx
+++ exercicios_react/src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
 
+// objeto chave/valor em que as chaves são os componentes e valores são funções
+const reducers = combineReducers({
+    field: () => ({ value: "Opa" })
+})
+
 ReactDOM.render(
-    <Field valor="Olá" />,
+    <Provider store={createStore(reducers)}>
+        <Field valor="Olá" />
+    </Provider>,
     document.getElementById('root')
 )
